c27f48fccb02193460190eafec452c74e7db4c41
Mihail Malakhov <m.malakhov@lan-project.ru>
2023-03-14 12:08:39 +0300

    решение задач

---
+++ trainings/3.0/firstHomework/36/index.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+let [n, ...matrix] = fs.readFileSync("input.txt", "utf8").split("\n");
+
+/**
+ *
+ * @param {string} n В первой строке дано одно число n — количество вершин в графе ( 1 ≤ n ≤ 500 ).
+ * @param {string[]} matrix Далее в n строках задан сам граф матрицей смежности.
+ * @returns {void} Если в иcходном графе нет цикла, то выведите «NO». Иначе, в первой строке выведите «YES», во второй строке выведите число k — количество вершин в цикле, а в третьей строке выведите k различных чисел — номера вершин, которые принадлежат циклу в порядке обхода (обход можно начинать с любой вершины цикла). Если циклов несколько, то выведите любой.
+ */
+
+const shortestWay = (n, matrix) => {
+  n = parseInt(n);
+  matrix = matrix
+    .filter((row) => row !== "")
+    .map((row) => row.split(" ").map((i) => parseInt(i)));
+  const [s, f] = matrix.pop();
+
+  const dist = new Array(n + 1).fill(-1);
+  dist[s] = 0;
+  bfs(s, n, matrix, dist);
+  fs.writeFileSync("output.txt", dist[f].toString());
+};
+
+const bfs = (start, n, matrix, dist) => {
+  const queue = [start];
+
+  while (queue.length > 0) {
+    const v = queue.shift();
+
+    for (let i = 1; i < n + 1; i++) {
+      if (matrix[v - 1][i - 1] && dist[i] == -1) {
+        queue.push(i);
+        dist[i] = dist[v] + 1;
+      }
+    }
+  }
+};
+
+shortestWay(n, matrix);
