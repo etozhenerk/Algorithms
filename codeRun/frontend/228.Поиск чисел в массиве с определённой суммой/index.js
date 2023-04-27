8ef5b29e40761f5a7ef4a31febda12a58ebe978c
Mikhail <etozhenerk@yandex.ru>
2023-04-27 23:32:03 +0300

    убрал задачи сезона, убрал тестовые задачи шри

--- codeRun/frontend/228.Поиск чисел в массиве с определённой суммой/index.js
+++
@@ -1,9 +0,0 @@
-/** @returns Boolean */
-module.exports = function (nums, k) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i; j < nums.length; j++) {
-      if (nums[i] + nums[j] === k && j !== i) return true;
-    }
-  }
-  return false;
-};
